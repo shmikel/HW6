d71e58801b1403d9d825031a1dc2afd4e13a311b
shmikel_home <shmikel0012@gmail.com>
2016-01-31 22:57:47 +0300

    1

---
+++ HW6/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task t = Task.Factory.StartNew(() => MessageBox.Show(" QuickCall - это:\n\n -Универсальный поиск организаций и учреждений.\n -Мгновенное добавление номеров в телефонную книгу android.\n\nЧто-то ещё? Пишите!"));
+        }
+    }
+}
